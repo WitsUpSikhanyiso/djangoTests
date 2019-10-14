8da46fc04e1930fbb3bf29f5d8e9e0879a3992ea
WitsUpSikhanyiso <1428523@students.wits.ac.za>
2019-10-14 23:42:55 +0100

    Update tests.py

--- tutortracker/tests.py
+++ tutortracker/tests.py
@@ -2,9 +2,6 @@ from django.test import SimpleTestCase , TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
 
-
-
-
 class SimpleTests(SimpleTestCase):
 	def test_home_page_status_code(self):
 		response = self.client.get('/tutortracker/')
